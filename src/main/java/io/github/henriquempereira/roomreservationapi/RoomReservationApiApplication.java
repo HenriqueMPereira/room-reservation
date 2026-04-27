63b760facac2d03799378a33fbcd9f15efd69b4e
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2026-04-27 16:01:24 -0300

    Primeiro commit

---
+++ src/main/java/io/github/henriquempereira/roomreservationapi/RoomReservationApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.henriquempereira.roomreservationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/henriquempereira/roomreservationapi/RoomReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henriquempereira.roomreservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
