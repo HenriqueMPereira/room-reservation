08a97a6ca2cca82ecb7b7658df6a2e2e5338d5c3
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2026-04-28 11:43:14 -0300

    feat: criar entidades principais de domínio e validações.

---
+++ src/main/java/io/github/henriquempereira/roomreservationapi/room/Room.java
@@ -0,0 +1,38 @@
+package io.github.henriquempereira.roomreservationapi.room;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+/**
+ * Representa uma sala física disponível para locação ou uso.
+ * <p>
+ *     Contém as especificações operacionais do espaço, incluindo
+ *     limite de capacidade e disponibilidade atual.
+ * </p>
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @NotNull(message = "A sala deve ter um nome.")
+    @Column(name = "name")
+    private String roomName;
+
+    @NotNull(message = "Capacidade é obrigatória.")
+    @Positive(message = "Capacidade deve ser positiva.")
+    private int capacity;
+
+    @NotNull(message = "A sala deve ter status 'ATIVA' ou 'INATIVA'.")
+    @Column(name = "status")
+    private RoomStatus roomStatus;
+}
---
+++ src/main/java/io/github/henriquempereira/roomreservationapi/user/User.java
@@ -0,0 +1,34 @@
+package io.github.henriquempereira.roomreservationapi.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+/**
+ * Representa um usuário do sistema de reservas.
+ * <p>
+ *     Entidade responsável por armazenar os dados de identificação
+ *     dos clientes que podem solicitar agendamentos de salas.
+ * </p>
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull(message = "Usuário deve ter um nome.")
+    @Column(name = "name")
+    private String userName;
+
+    @CPF(message = "Usuário deve ter um CPF válido.")
+    @NotNull(message = "Usuário deve ter um CPF.")
+    private String cpf;
+}
